9e583234fe202083b8a1f1adccf6138a0a2fb144
davem <davem@DESKTOP-I04DH1K.admin.ad.cnm.edu>
2017-06-14 10:45:47 -0600

    returns zero to gcd and is printed with a and b

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -5,23 +5,29 @@ public class Gcd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			int result = gcd(a, b);
 			
-		
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
-		
-		System.out.printf("a =%d, b = %d %n", a, b);
+			System.out.printf("a =%d, b = %d, GCD = %d %n", a, b, result);
+			
 		}
-		
-		catch(NumberFormatException ex){
+
+		catch (NumberFormatException ex) {
 			ex.printStackTrace();
 			System.out.println("This application requires two integer values");
 		}
-		catch(ArrayIndexOutOfBoundsException ex){
-			
+
+		catch (ArrayIndexOutOfBoundsException ex) {
 			System.out.println("This application must have at least two integer values");
-			
 		}
+	}
+
+	public static int gcd(int a, int b) {
+		
+		return 0;
+
 	}
 
 }
