6fe6b9e92d80ce55ad323a1a3984b553d44ca73e
davem <davem@DESKTOP-I04DH1K.admin.ad.cnm.edu>
2017-06-14 11:14:43 -0600

    GCD working with while subtractions

--- src/edu/cnm/deepdive/euclid/Gcd.java
+++ src/edu/cnm/deepdive/euclid/Gcd.java
@@ -9,25 +9,35 @@ public class Gcd {
 			int a = Integer.parseInt(args[0]);
 			int b = Integer.parseInt(args[1]);
 			int result = gcd(a, b);
-			
-			System.out.printf("a =%d, b = %d, GCD = %d %n", a, b, result);
-			
-		}
 
+			System.out.printf("a =%d, b = %d, GCD = %d %n", a, b, result);
+		}
 		catch (NumberFormatException ex) {
 			ex.printStackTrace();
 			System.out.println("This application requires two integer values");
 		}
-
 		catch (ArrayIndexOutOfBoundsException ex) {
 			System.out.println("This application must have at least two integer values");
 		}
 	}
-
+	
 	public static int gcd(int a, int b) {
-		
-		return 0;
 
+		a = Math.abs(a);
+		b = Math.abs(b);
+
+		if (a == 0)
+			return b;
+		if (b == 0)
+			return a;
+
+		while (a != b) {
+			if (a > b)
+				a = a - b;
+			else
+				b = b - a;
+		}
+		return a;
 	}
 
 }
